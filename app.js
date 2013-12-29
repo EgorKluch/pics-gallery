d9a01e3f3ec206bad24c8df90ce6a5c932133e13
EgorKluch <EgorKluch@gmail.com>
2013-12-29 15:57:47 +0700

    Add simple express app for checking work

---
+++ app.js
@@ -0,0 +1,14 @@
+/**
+ * @author: EgorKluch (EgorKluch@gmail.com)
+ * @date: 29.12.13
+ */
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+app.listen(3000);
+console.log('Express started on port 3000');
