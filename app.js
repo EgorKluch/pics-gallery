25ccd96b4ffcf02e8eff6c23ac58118a06131ea4
EgorKluch <EgorKluch@gmail.com>
2013-12-29 16:03:13 +0700

    Add mysql module Add twig module Check its modules

--- app.js
+++ app.js
@@ -6,8 +6,32 @@
 var express = require('express');
 var app = express();
 
+var config = require('./config/config');
+
+var mysql = require('mysql');
+var connection = mysql.createConnection(config.mysql);
+connection.connect(function(err) {
+  //if (err) throw err;
+  console.log('Mysql is connected!');
+});
+
+app.configure(function(){
+  app.set('views', __dirname + '/templates');
+  app.set('view engine', 'twig');
+
+  // This section is optional and can be used to configure twig.
+  app.set('twig options', {
+    strict_variables: true
+  });
+});
+
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  connection.query('SELECT 1', function (err, rows) {
+    console.log(rows);
+    res.render('index', {
+      message : "Hello World"
+    });
+  });
 });
 
 app.listen(3000);
---
+++ config/config.sample.js
@@ -0,0 +1,13 @@
+/**
+ * @author: EgorKluch (EgorKluch@gmail.com)
+ * @date: 29.12.13
+ */
+
+module.exports = {
+  mysql: {
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'mydb'
+  }
+};
