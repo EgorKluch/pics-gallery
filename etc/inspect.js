29d213985780d4e888a4aabd09727bc02d55f681
EgorKluch <egorkluch@sotal.tv>
2014-01-25 18:28:54 +0700

    Delete not need  files Edit gitignore

--- etc/inspect.js
+++ etc/inspect.js
@@ -1,1 +1,12 @@
+/**
+ * @author: EgorKluch (EgorKluch@gmail.com)
+ * @date: 25.01.14
+ */
+
 var __dirname = '';
+var obj = {
+  repeatePassword: {},
+  ngTarget: {},
+  ngType: {},
+  ngError: {}
+};cc
